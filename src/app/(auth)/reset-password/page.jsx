691786f1ad9dfd2f04e779da23af5bf6cfc2fe43
Akinfenwa Taiwo Christopher <101477417+AChris-T@users.noreply.github.com>
2025-07-30 15:11:19 +0100

    feat:added project setup

--- src/app/(auth)/reset-password/page.jsx
+++ src/app/(auth)/reset-password/page.jsx
@@ -1,12 +1,15 @@
+// app/(auth)/reset-password/page.tsx
 'use client';
+
 import ResetPasswordForm from '@/components/auth/ResetPasswordForm';
 import { useSearchParams } from 'next/navigation';
-import React from 'react';
+import React, { Suspense } from 'react';
 
-export default function resetpassword() {
+function ResetPasswordPage() {
   const searchParams = useSearchParams();
   const email = searchParams.get('email');
   const otp = searchParams.get('otp');
+
   if (!email || !otp) {
     return (
       <div className="text-red-600 text-center mt-10">
@@ -14,5 +17,14 @@ export default function resetpassword() {
       </div>
     );
   }
+
   return <ResetPasswordForm email={email} otp={otp} />;
+}
+
+export default function PageWrapper() {
+  return (
+    <Suspense fallback={<div className="text-center mt-10">Loading...</div>}>
+      <ResetPasswordPage />
+    </Suspense>
+  );
 }
